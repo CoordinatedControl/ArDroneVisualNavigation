fd4e639b28a8cc55c8d7b8a65edd619b7836b125
Nikhil Venkatesh <nivenkatesh@paypal.com>
2016-10-23 04:15:53 +0800

    Modified Control Values

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var Copterface = require('./lib/copterface');
 var arDrone = require('ar-drone');
 var client = arDrone.createClient({ip: '192.168.1.10'});
+//client.disableEmergency()
 client.takeoff();
 console.log('Connected to client');
 var pngStream = client.getPngStream();
@@ -10,12 +11,14 @@ var Controller   = require('node-pid-controller');
 
 var ver_ctrl = new Controller(0.3, 0.01, 0.1)
   , hor_ctrl = new Controller(0.4, 0.01, 0.1)
+  , front_ctrl = new Controller(0.1, 0.01, 0.2)
   ;
 
 var copterface = Copterface(pngStream, function(info){
 	console.log(info);
 	var face = info.rects;
 	var im = info.image;
+	var targetWidth = 100;
 	face.centerX = face.x + face.width * 0.5;
 	face.centerY = face.y + face.height * 0.5;
 
@@ -24,22 +27,31 @@ var copterface = Copterface(pngStream, function(info){
 
 	var heightAmount = -( face.centerY - centerY ) / centerY;
 	var turnAmount = -( face.centerX - centerX ) / centerX;
+	var forwardAmount = ( targetWidth - face.width ) / 500;
 
 	heightAmount = ver_ctrl.update(-heightAmount);
 	turnAmount   = hor_ctrl.update(-turnAmount);
+	forwardAmount = front_ctrl.update(-forwardAmount);
 
 	var lim = 0.1;
-	if( Math.abs( turnAmount ) > lim || Math.abs( heightAmount ) > lim ){
-		console.log( "turning" + turnAmount );
-		if( turnAmount < 0 ) 
+	if (Math.abs(face.width) > targetWidth){
+		client.stop();
+	} else if( Math.abs( turnAmount ) > lim || Math.abs( heightAmount ) > lim ){
+		console.log( "forward" + forwardAmount );
+		/*if( turnAmount < 0 ) 
 			client.clockwise(Math.abs(turnAmount));
 		else 
-			client.counterClockwise(turnAmount);
+			client.counterClockwise(turnAmount);*/
 	
 		if(  heightAmount < 0 )
 			client.down(Math.abs(heightAmount));
 		else 
 			client.up(heightAmount);
+
+		if ( forwardAmount < 0 )
+			client.back(Math.abs(forwardAmount));
+		else
+			client.front(Math.abs(forwardAmount));
 	} else {
 		client.stop();
 	}
@@ -51,7 +63,7 @@ var copterface = Copterface(pngStream, function(info){
 
 setTimeout(function(){
 	client.land();
-}, 20000);
+}, 40000);
 
 process.on("SIGINT", function(){
 	client.land();
