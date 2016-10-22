0a353da0dc8647ff8954f0577b75180700cbcd49
Nikhil Venkatesh <nivenkatesh@paypal.com>
2016-10-23 05:18:58 +0800

    Code Refactoring

--- index.js
+++ index.js
@@ -1,56 +1,55 @@
+// Imports
 var ImageProcessing = require('./lib/ImageProcessing');
 var arDrone = require('ar-drone');
 var client = arDrone.createClient({ip: '192.168.1.10'});
 console.log('Connected to client');
 var pngStream = client.getPngStream();
-var Controller   = require('node-pid-controller');
+var Controller = require('node-pid-controller');
 
-var ver_ctrl = new Controller(0.3, 0.01, 0.1)
-  , hor_ctrl = new Controller(0.4, 0.01, 0.1)
-  , front_ctrl = new Controller(0.1, 0.01, 0.2)
-  ;
+// Initialize PID Controllers
+var verticalPidController = new Controller(0.3, 0.01, 0.1), 
+	spinPidController = new Controller(0.4, 0.01, 0.1), 
+	horizontalPidController = new Controller(0.1, 0.01, 0.2)
+	;
 
-client.takeoff();
-
+// Initialize Image Processing
 var imageProcessing = ImageProcessing(pngStream, function(info){
 	console.log(info);
-	var face = info.rects;
+	var target = info.rects;
 	var im = info.image;
 	var targetWidth = 100;
-	face.centerX = face.x + face.width * 0.5;
-	face.centerY = face.y + face.height * 0.5;
+	target.centerX = target.x + target.width * 0.5;
+	target.centerY = target.y + target.height * 0.5;
 
 	var centerX = im.width() * 0.5;
 	var centerY = im.height() * 0.5;
 
-	var heightAmount = -( face.centerY - centerY ) / centerY;
-	var turnAmount = -( face.centerX - centerX ) / centerX;
-	var forwardAmount = ( targetWidth - face.width ) / 500;
+	var heightAmount = -( target.centerY - centerY ) / centerY;
+	var turnAmount = -( target.centerX - centerX ) / centerX;
+	var forwardAmount = ( targetWidth - target.width ) / 500;
 
-	heightAmount = ver_ctrl.update(-heightAmount);
-	turnAmount   = hor_ctrl.update(-turnAmount);
-	forwardAmount = front_ctrl.update(-forwardAmount);
+	heightAmount = verticalPidController.update(-heightAmount);
+	turnAmount   = spinPidController.update(-turnAmount);
+	forwardAmount = horizontalPidController.update(-forwardAmount);
 
 	var lim = 0.1;
-	if (Math.abs(face.width) > targetWidth){
+	if (Math.abs(target.width) > targetWidth){
 		client.stop();
 	} else if( Math.abs( turnAmount ) > lim || Math.abs( heightAmount ) > lim ){
 		console.log( "forward" + forwardAmount );
-		/*if( turnAmount < 0 ) 
-			client.clockwise(Math.abs(turnAmount));
-		else 
-			client.counterClockwise(turnAmount);*/
 	
-		if(  heightAmount < 0 )
+		if(heightAmount < 0) {
 			client.down(Math.abs(heightAmount));
-		else 
+		} else {
 			client.up(heightAmount);
-
-		if ( forwardAmount < 0 )
+		}
+		if(forwardAmount < 0) {
 			client.back(Math.abs(forwardAmount));
-		else
+		}
+		else{
 			client.front(Math.abs(forwardAmount));
-	} else {
+		}
+	} else{
 		client.stop();
 	}
 
@@ -58,10 +57,7 @@ var imageProcessing = ImageProcessing(pngStream, function(info){
 	dt = dt * 2000;
 });
 
-setTimeout(function(){
-	client.land();
-}, 40000);
-
+// FAILSAFES
 process.on("SIGINT", function(){
 	client.land();
 	setTimeout(function() {
@@ -83,4 +79,8 @@ process.on("uncaughtException", function(){
 	}, 1000);
 });
 
+// Takeoff drone
+client.takeoff();
+
+//Start Image Processing
 imageProcessing.start();
